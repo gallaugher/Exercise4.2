ff27112eaa6bea4fa2b1036b767a0fd73ff9984a
Gallaugher <john.gallaugher@bc.edu>
2017-09-27 11:07:21 -0400

    Completed imageExercise from Chapter 4.2 in http://gallaugher.com/swift

--- imageExercise/ViewController.swift
+++ imageExercise/ViewController.swift
@@ -9,115 +9,83 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var imageToPunch: UIImageView!
-    var originalBounds: CGRect!
-    var originalFrame: CGRect!
-    var originalX: CGFloat!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    var originalImageBounds: CGRect!
+    var originalImageFrame: CGRect!
+    var originalViewCenterX: CGFloat!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        originalBounds = imageToPunch.bounds
-        originalFrame = imageToPunch.frame
-        originalX = self.view.center.x
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        originalImageBounds = imageView.bounds
+        originalImageFrame = imageView.frame
+        originalViewCenterX = self.view.center.x
     }
 
     @IBAction func resetPressed(_ sender: Any) {
-        imageToPunch.bounds = originalBounds
-        imageToPunch.frame = originalFrame
-        self.view.center.x = originalX
-        imageToPunch.alpha = 1.0
-        imageToPunch.transform = CGAffineTransform.identity
+        imageView.bounds = originalImageBounds
+        imageView.frame = originalImageFrame
+        self.view.center.x = originalViewCenterX
+        imageView.alpha = 1.0
+        
+        // .identity will clear any changes made using transform
+        imageView.transform = CGAffineTransform.identity
         self.view.backgroundColor = UIColor.white
     }
-
+    
     @IBAction func increased60Pressed(_ sender: UIButton) {
+        let newBounds = CGRect(x: imageView.bounds.origin.x - 60, y: imageView.bounds.origin.y - 60, width: imageView.bounds.width + 60, height: imageView.bounds.height + 60)
         
-        let expandBy: CGFloat = 60
-        let newBounds = CGRect(x: self.imageToPunch.bounds.origin.x - expandBy, y: self.imageToPunch.bounds.origin.y - expandBy, width: self.imageToPunch.bounds.size.width + expandBy, height: self.imageToPunch.bounds.size.height + expandBy)
-        
-        // Without Spring
-                UIView.animate(withDuration: 0.25, delay: 0.0, options: [], animations: {
-                        self.imageToPunch.bounds = newBounds}
-                    , completion: nil )
-        
-        // With Spring
-//        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: {
-//                self.imageToPunch.bounds = newBounds}
-//            , completion: nil )
+        UIView.animate(withDuration: 0.5, animations: { self.imageView.bounds = newBounds } )
     }
 
-    @IBAction func pushRightPressed(_ sender: UIButton) {
+    @IBAction func decreaseBy60Pressed(_ sender: UIButton) {
+        let newBounds = CGRect(x: imageView.bounds.origin.x + 60, y: imageView.bounds.origin.y + 60, width: imageView.bounds.width - 60, height: imageView.bounds.height - 60)
         
-        UIView.animate(withDuration: 0.25, delay: 0.0, options: [], animations: {
-            self.imageToPunch.frame.origin.x = self.view.frame.size.width}
-            , completion: nil )
+        UIView.animate(withDuration: 0.5, animations: { self.imageView.bounds = newBounds } )
+    }
+    
+    @IBAction func pushRightPressed(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.imageView.frame.origin.x = self.view.frame.size.width})
     }
     
     @IBAction func slideScreen100Pressed(_ sender: UIButton) {
-        UIView.animate(withDuration: 0.25, delay: 0.0, options: [], animations: {
-            self.view.center.x += 100}
-            , completion: nil )
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.center.x += 100})
     }
     
     @IBAction func fadePressed(_ sender: UIButton) {
-        
-        UIView.animate(withDuration: 5.0, delay: 0.0, options: [], animations: {
-            self.imageToPunch.alpha = 0
-        }
-            , completion: nil )
-
+        UIView.animate(withDuration: 5.0, animations: { self.imageView.alpha = 0 })
     }
     
-    func degreesToRadians(degrees: Int) -> CGFloat {
-        return CGFloat(Double(degrees) * (.pi / 180))
-    }
     
     @IBAction func doA180Pressed(_ sender: UIButton) {
-        
+        // CGAffineTransformTransform(rotationAngel: works off of radians, so
+        // if you're like me & think degrees instead, the code below will process
+        // a degree rotation. Note a 360 is different, it'll involve a new set of technologies, as will continued spin.  I can cover that in a future class, if you'd like.
         let rotateInDegrees: CGFloat = 180
         
-        UIView.animate(withDuration: 0.5, delay: 0.0, options: [], animations: {
-            self.imageToPunch.transform = CGAffineTransform(rotationAngle:(rotateInDegrees * CGFloat(M_PI)) / 180.0)}
-            , completion: nil )
-        
-//        for _ in 1...4 {
-//        UIView.animate(withDuration: 3, delay: 0.0, options: [], animations: {
-//            self.imageToPunch.transform = CGAffineTransform(rotationAngle: self.degreesToRadians(degrees: 180))}
-//            , completion: nil )
-//        
-//        UIView.animate(withDuration: 3, delay: 2.95, options: [], animations: {
-//            self.imageToPunch.transform = CGAffineTransform(rotationAngle: self.degreesToRadians(degrees: 360))}
-//            , completion: nil )
-//        }
-
-        
+        UIView.animate(withDuration: 0.5, animations:
+            { self.imageView.transform = CGAffineTransform(rotationAngle:
+                (rotateInDegrees * CGFloat(Double.pi)) / 180.0)})
     }
     
     @IBAction func changeBackgroundColorPressed(_ sender: UIButton) {
-        UIView.animate(withDuration: 2.0, delay: 0.0, options: [], animations: {
-            self.view.backgroundColor = UIColor.red
-        }
-            , completion: nil )
+        UIView.animate(withDuration: 2.0, animations: {
+            self.view.backgroundColor = UIColor.red })
     }
     
-    @IBAction func moveLogoPressed(_ sender: UIButton) {
-        
-        if self.imageToPunch.frame.origin.x >= self.view.frame.size.width {
-            self.imageToPunch.frame.origin.x = 0 - self.imageToPunch.bounds.width
+    @IBAction func loopLogoPressed(_ sender: UIButton) {
+        if self.imageView.frame.origin.x >= self.view.frame.size.width {
+            self.imageView.frame.origin.x = 0 - self.imageView.bounds.width
         }
         
         let moveBy:CGFloat = 100
-        UIView.animate(withDuration: 0.25, delay: 0.0, options: [], animations: {
-            self.imageToPunch.frame.origin.x = self.imageToPunch.frame.origin.x + moveBy}
-            , completion: nil )
+        UIView.animate(withDuration: 0.25, animations:
+            { self.imageView.frame.origin.x = self.imageView.frame.origin.x +
+                moveBy})
     }
+    
 }
 
